a7156c8d688f73a1c9482172ea481b9ee644a032
Munish Chandel <cancerian0684@gmail.com>
2019-11-26 21:30:05 +0530

    initial project checkin

---
+++ src/test/java/com/shunya/tutorial/webtest/DemoAppTests.java
@@ -0,0 +1,13 @@
+package com.shunya.tutorial.webtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
